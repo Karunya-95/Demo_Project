0151090a9203e7df3282513e2c183a381b8f340c
Dhivya <dhivya.ravichandran@ejyle.com>
2017-06-22 15:09:41 +0530

    Add project files.

---
+++ Demo_Project1/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿
+//old
+using Demo_Project1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Demo_Project1.Controllers
+{
+    public class HomeController : Controller
+    {
+		public ActionResult Index()
+        {
+            return View();
+        }
+
+		//For Login Controller
+		public ActionResult ValidateUser(string user_name, string password)
+		{
+			Int32 validateuser = 0;
+			Register Validate = new Register();
+			validateuser = Validate.ValidateUser(user_name, password);
+			if (validateuser == 1)
+            {
+                Session["UseerId"] = user_name;
+                return Json(new { status = "Success", message = "Success" });
+            }	
+			else
+				return Json(new { status = "Error", message = "Failed" });
+		}
+
+		//For Register Controller
+		public JsonResult SaveDetailedInfo(string user_name, DateTime dob, string mail_id, string password, string confirm_password, string phone_number, string Address)
+		{
+
+			Register Log = new Register();
+			Log.Savedata(user_name, dob, mail_id, password, confirm_password, phone_number, Address);
+			return Json(new { status = "Success", message = "Success" });
+		}
+
+		public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+		public ActionResult Instruction()
+		{
+			
+			return View();
+		}
+        //For Questionnaire Page
+		public ActionResult Questionnaire()
+		{
+			Register Reg = new Register();
+			List<questions> listids = new List<questions>();
+			listids = Reg.Questionniare();
+			return View(listids);
+		}
+        //For Generating the result
+        public ActionResult GenerateResult(List<SelectListItem> list)
+        {
+            int rightcount = 0;
+            int wrongans = 0;
+            Register Reg = new Register();
+            List<questions> listids = new List<questions>();
+            listids = Reg.Questionniare();
+            for (int i = 0; i <= 19; i++)
+            {
+                if (listids[i].QuestionId == Convert.ToInt32(list[i].Text) && listids[i].Correct_Answer == list[i].Value)
+                {
+                    rightcount += 1;
+                }
+                else
+                {
+                    wrongans += 1;
+                }
+            }
+            string User_Id = Session["UseerId"].ToString();
+            Reg.SaveDataFromUser(list, listids, User_Id);
+            return Json(new { rAns = rightcount, wans = wrongans });
+        }       
+	}
+}
---
+++ Demo_Project1/Models/Register.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Collections;
+using System.Web.Mvc;
+
+namespace Demo_Project1.Models
+{
+	public class Register
+	{
+		//For Login Model
+		public int ValidateUser(string user_name, string password)
+		{
+			string strcon = ConfigurationManager.ConnectionStrings["Dbconnection"].ConnectionString;
+			int UserTypeId;
+			SqlConnection con = new SqlConnection(strcon);
+			SqlCommand cmd = new SqlCommand();
+			try
+			{
+				cmd.Connection = con;
+				cmd.CommandType = System.Data.CommandType.StoredProcedure;
+				cmd.CommandText = "LoginUser";
+				cmd.Parameters.Add("@user_name", SqlDbType.VarChar).Value = user_name;
+				cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = password;
+				cmd.Parameters.Add("@result", SqlDbType.Int);
+				cmd.Parameters["@result"].Direction = ParameterDirection.Output;
+				con.Open();
+				cmd.ExecuteNonQuery();
+				UserTypeId = Convert.ToInt32(cmd.Parameters["@result"].Value);
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+			finally
+			{
+				con.Close();
+			}
+			return UserTypeId;
+		}
+
+		//For Register Model
+		public void Savedata(string user_name, DateTime dob, string mail_id, string password, string confirm_password, string phone_number, string Address)
+		{
+
+			string strcon = ConfigurationManager.ConnectionStrings["Dbconnection"].ConnectionString;
+			SqlConnection DbConnection = new SqlConnection(strcon);
+			DbConnection.Open();
+			SqlCommand command = new SqlCommand("INSERT into Registration_Table (user_name,dob,mail_id,password,confirm_password,phone_number,Address) VALUES (@user_name,@dob,@mail_id,@password,@confirm_password,@phone_number,@Address)", DbConnection);
+			command.Parameters.Add(new SqlParameter("@user_name", user_name));
+			command.Parameters.Add(new SqlParameter("@dob", dob));
+			command.Parameters.Add(new SqlParameter("@mail_id", mail_id));
+			command.Parameters.Add(new SqlParameter("@password", password));
+			command.Parameters.Add(new SqlParameter("@confirm_password", confirm_password));
+			command.Parameters.Add(new SqlParameter("@phone_number", phone_number));
+			command.Parameters.Add(new SqlParameter("@Address", Address));
+			command.ExecuteNonQuery();
+		}
+
+		//For Questionniare Model
+		public List<questions> Questionniare()
+		{
+			List<questions> listid = new List<questions>();
+			
+			string strcon = ConfigurationManager.ConnectionStrings["Dbconnection"].ConnectionString;
+			SqlConnection conn = new SqlConnection(strcon);
+			string queryString = " select * from QuestionsTable";
+			conn.Open();
+			SqlCommand cmd = new SqlCommand(queryString, conn);
+			SqlDataReader dr = cmd.ExecuteReader();
+			while (dr.Read())
+			{
+				questions question = new questions();
+				question.QuestionId = Convert.ToInt32(dr["QuestionId"]);
+				question.Questions = dr["Questions"].ToString();
+				question.OptionA = dr["OptionA"].ToString();
+				question.OptionB = dr["OptionB"].ToString();
+				question.OptionC = dr["OptionC"].ToString();
+				question.OptionD = dr["OptionD"].ToString();
+				question.OptionE = dr["OptionE"].ToString();
+				question.Correct_Answer = dr["Correct_Answer"].ToString();
+				listid.Add(question);
+			}
+			return listid;
+		}
+
+        //For Save Table
+        public void SaveDataFromUser(List<SelectListItem> list, List<questions> listids, string User_Id)
+        {
+
+            string strcon = ConfigurationManager.ConnectionStrings["Dbconnection"].ConnectionString;
+            SqlConnection DbConnection = new SqlConnection(strcon);
+            DbConnection.Open();
+
+            for (int i = 0; i <= 19; i++)
+            {
+                SqlCommand command = new SqlCommand("insert into Save_Table (User_Id , QuestionId ,Questions, Correct_Answer) values('" + User_Id + "'," + list[i].Text + ",'" + listids[i].Questions + "','" + list[i].Value + "')", DbConnection);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
+	
+				
+
